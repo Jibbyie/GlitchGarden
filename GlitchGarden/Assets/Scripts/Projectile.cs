fb7984c49a5ad18015490a4cfb094fa045bc9056
Jibril Abdulazeez <jibbyie@gmail.com>
2021-04-10 12:18:03 +0100

    Health & Damage System

--- GlitchGarden/Assets/Scripts/Projectile.cs
+++ GlitchGarden/Assets/Scripts/Projectile.cs
@@ -6,10 +6,18 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] float projectileSpeed = 5f;
     [SerializeField] float rotationSpeed = 5f;
+    [SerializeField] float damage = 50f;
  
     void Update()
     {
         transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
         transform.Rotate(Vector2.right * rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+
+        health.DealDamage(damage);
     }
 }
